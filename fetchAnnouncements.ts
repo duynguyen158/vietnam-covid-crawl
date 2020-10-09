09f3b55e0d72868e29197e90ee684d61c538e8c5
duynguyen158 <57234880+duynguyen158@users.noreply.github.com>
2020-10-09 13:30:55 +0700

    Separate fetches from main

---
+++ fetchAnnouncements.ts
@@ -0,0 +1,64 @@
+import cheerio from "cheerio";
+import { fetchData, validate } from "./utils";
+
+async function fetchAnnouncements() {
+    let url = "https://ncov.moh.gov.vn/web/guest/dong-thoi-gian";
+    let carryOn = true;
+
+    const data = new Array<Object>();
+
+    // Announcement timeline is paginated, so a do-while loop takes us to next page.
+    do {
+        // Fetch
+        const response = await fetchData(url);
+
+        // Validate
+        const html = validate(response);
+        if (html === undefined) break;
+
+        // Mount HTML page to root element
+        const $ = cheerio.load(html);
+
+        // Container (list) of all announcements in our current page
+        const timeline = $(".timeline-sec > ul");
+
+        // Loop through all list elements
+        const prefix = "li > .timeline > .timeline-detail";
+        timeline.each((_, d: cheerio.Element) => {
+            // Timestamp (HH:MM d/m/Y, GMT+7)
+            const timestamp = $(d)
+                .find(`${prefix} > .timeline-head > h3`)
+                .text();
+
+            // Content of each announcement
+            const paragraphs: string[] = $(d)
+                .find(`${prefix} > .timeline-content > p`)
+                .map((_, d: cheerio.Element) => $(d).text())
+                .get();
+
+            data.push({ timestamp, paragraphs });
+        });
+
+        // Button to next page
+        const nextButton = $(".lfr-pagination-buttons.pager > li")[1];
+
+        // Obtain URL to next page.
+        // If there is a valid one, continue loop. Otherwise, break.
+        if ($(nextButton).hasClass("disabled")) {
+            carryOn = false;
+        } else {
+            const href = $(nextButton).find("a").attr("href");
+            if (href === undefined) {
+                console.log(
+                    "IMPORTANT: URL to next page is compromised. Fetching will stop. Data might be incomplete."
+                );
+                break;
+            }
+            url = href;
+        }
+    } while (carryOn);
+
+    return data;
+}
+
+export default fetchAnnouncements;
---
+++ fetchCases.ts
@@ -0,0 +1,32 @@
+import cheerio from "cheerio";
+import { fetchData, validate } from "./utils";
+
+async function fetchCases() {
+    const url = "https://ncov.moh.gov.vn";
+
+    // Fetch
+    const response = await fetchData(url);
+
+    // Validate
+    const html = validate(response);
+    if (html === undefined) return;
+
+    // Mount HTML page to root element
+    const $ = cheerio.load(html);
+
+    const data = new Array<string[]>();
+    const table = $(".table.table-striped.table-covid19 > tbody > tr");
+
+    // Loop through all table rows
+    table.each((_, d: cheerio.Element) => {
+        const datum: string[] = $(d)
+            .find("td")
+            .map((_, d: cheerio.Element) => $(d).text())
+            .get();
+        data.push(datum);
+    });
+
+    return data;
+}
+
+export default fetchCases;
