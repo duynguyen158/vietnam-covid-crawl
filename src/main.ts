5ceca53bc0156941858137da84691b801045541b
duynguyen158 <57234880+duynguyen158@users.noreply.github.com>
2020-10-09 16:49:21 +0700

    Save fetched to local

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,36 @@
 import fetchCases from "./fetchCases";
 import fetchAnnouncements from "./fetchAnns";
+import { Worker } from "worker_threads";
 
+// .js, not .ts. The action is in the build folder
+const casesWorkDir = __dirname + "/casesWorker.js";
+const announcementsWorkDir = __dirname + "/annsWorker.js";
+
+// Save case list to local data folder. In the future, may use
+// worker_threads to process & push to a remote database.
 fetchCases().then((response) => {
-    // Print out case list for the time being. In the future,
-    // may use worker_threads to process & push to a remote database.
-    console.log(response);
+    // Unlike fetchAnnouncements, fetchCases might return undefined
+    if (response) {
+        // Start worker
+        const worker = new Worker(casesWorkDir);
+        console.log("Sending case data to casesWorker...");
+
+        // Send to cases worker thread
+        worker.postMessage(response);
+
+        // Listen to message from cases worker
+        worker.on("message", (message) => console.log(message));
+    }
 });
 
 fetchAnnouncements().then((response) => {
-    // Print out announcement list for the time being. In the future,
-    // may use worker_threads to process & push to a remote database.
-    console.log(response);
+    // Start worker
+    const worker = new Worker(announcementsWorkDir);
+    console.log("Sending case data to casesWorker...");
+
+    // Send to cases worker thread
+    worker.postMessage(response);
+
+    // Listen to message from cases worker
+    worker.on("message", (message) => console.log(message));
 });
